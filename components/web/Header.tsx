543e375d8de4fcfc6b638c6a70c273d0f774c090
carrington-115 <frumarkcarrington@gmail.com>
2026-01-07 11:04:51 +0530

    configured supabase lib elements

--- components/web/Header.tsx
+++ components/web/Header.tsx
@@ -6,7 +6,10 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="absolute left-0 right-0 top-4 px-4 flex items-center justify-between">
-      <Link href="/" className="flex items-center gap-2">
+      <Link
+        href="/"
+        className="flex items-center gap-2 text-black-500/70 hover:text-black-black-500/100"
+      >
         <Image src={LogoTitle} alt="Logo" width={36} height={36} />
         <h2 className="text-xl font-bold">AgroSmart</h2>
       </Link>
