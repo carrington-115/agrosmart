9ee9938638c676f6ba7c0639b9706f69a98097f0
carrington-115 <frumarkcarrington@gmail.com>
2026-01-11 10:31:05 +0530

    fixed the issue of less than 0 sensors

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   AnalyticsCard,
   DashboardHeader,
@@ -6,6 +9,7 @@ import {
 } from "@/components/web";
 import { analyticsProps } from "@/lib/types";
 import { CloudSun, Droplet, TreeDeciduous } from "lucide-react";
+import { useState } from "react";
 
 const dataAnalysisContent: analyticsProps[] = [
   {
@@ -43,23 +47,38 @@ const dataAnalysisContent: analyticsProps[] = [
 ];
 
 export default function Dashboard() {
+  const [sensors, setSensors] = useState<number>(1);
   return (
     <div className="max-w-[100%] overflow-x-hidden">
       <DashboardHeader />
-      <section className="max-w-[100%] mx-0 px-4 py-5 overflow-x-hidden">
-        <div className="w-full flex flex-col gap-4">
-          <h2 className="text-2xl font-semibold">SSSP Data Readings</h2>
-          <div className="w-full flex justify-between">
-            {dataAnalysisContent.map((item) => (
-              <AnalyticsCard key={item.title} {...item} />
-            ))}
+      <>
+        {sensors > 0 ? (
+          <>
+            <section className="max-w-[100%] mx-0 px-4 py-5 overflow-x-hidden">
+              <div className="w-full flex flex-col gap-4">
+                <h2 className="text-2xl font-semibold">SSSP Data Readings</h2>
+                <div className="w-full flex justify-between">
+                  {dataAnalysisContent.map((item) => (
+                    <AnalyticsCard key={item.title} {...item} />
+                  ))}
+                </div>
+              </div>
+            </section>
+            <section className="max-w-[100%] overflow-x-auto w-[100%] px-4 mx-auto flex justify-center gap-6 mb-20">
+              <MonitoringLineChart />
+              <Recommendations />
+            </section>
+          </>
+        ) : (
+          <div className="w-full h-screen flex justify-center items-center">
+            <div className="flex flex-col gap-3 items-center">
+              <h1 className="text-2xl font-bold">Get Started</h1>
+              <p>It's a bit quiet in here. Let's get the ball rolling.</p>
+              <Button>Get Started</Button>
+            </div>
           </div>
-        </div>
-      </section>
-      <section className="max-w-[100%] overflow-x-auto w-[100%] px-4 mx-auto flex justify-center gap-6 mb-20">
-        <MonitoringLineChart />
-        <Recommendations />
-      </section>
+        )}
+      </>
     </div>
   );
 }
